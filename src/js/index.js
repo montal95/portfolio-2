11b09d6ffe255e4dda6704044229aa6377087217
Sam M <sammontalvojr@gmail.com>
2019-05-08 13:06:59 -0500

    responsiveness on all pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import "../scss/main.scss";
+
+let icon = document.getElementById("menu-icon");
+let menu = document.getElementById("menu");
+let body = document.body;
+
+icon.addEventListener("click", function() {
+  let x;
+  let show;
+  if (icon.innerHTML === '<i class="fa fa-bars"></i>'){
+    x = '<i class="fa fa-times"></i>';
+    icon.innerHTML = x;
+    body.classList.add("menu-active");
+  } else {
+    x = '<i class="fa fa-bars"></i>';
+    icon.innerHTML = x;
+    body.classList.remove('menu-active');
+  }
+  console.log(menu);
+});
