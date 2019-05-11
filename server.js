ea7bd11ed076b02dd7656c4ce2ac78058b1e29ab
Sam M <sammontalvojr@gmail.com>
2019-05-11 11:49:48 -0500

    express server setup for Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+//create the express app
+const app = express();
+//create all middleware to hander serving the app
+app.use("/", serveStatic(path.join(_dirname, "/public")));
+//serve the index file by default
+app.get("/", function(req, res) {
+  res.sendFile(_dirname + "/src/index.html");
+});
+//create a default port to serve the app
+const port = process.env.PORT || 5000;
+app.listen(port);
+//log in to feedback that this is running
+console.log("Server started on port " + port);
